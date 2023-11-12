e11f1a1b2b445f95d1a9d32054d202068c236000
Nadia <marianagomez427@gmail.com>
2023-11-12 05:02:53 -0300

    Correccion tamaño del logo y media queries

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-
-require("dotenv").config();
-const express = require('express');
-const path = require ('path');
-
+const express = require ('express');
 const app = express();
-app.use(express.static('public'));
+const path = require('path')
 
-const port = process.env.PORT || 4000 ;
+const PORT = 3030
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/home.html");
-  });
+/*app.get('/',(req, res) => {
+    console.log();
+    res.send("TamoActivo?")
+})*/
 
-  app.get('/register', (req, res) => {
-     res.sendFile(__dirname + "/views/register.html");
-  });
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.get('/login', (req, res) => {
-    res.sendFile(__dirname + "/views/login.html");
- });
 
-const publicPath = path.resolve (__dirname, './public');
-app.use( express.static(publicPath));
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve('./views/index.html'));
+})
+app.get('/babbage',(req, res) => {
+    res.sendFile(path.resolve('./views/babbage.html'));
+})
+app.get('/lovelace',(req, res) => {
+    res.sendFile(path.resolve('./views/lovelace.html'));
+})
+app.get('/berners-lee',(req, res) => {
+    res.sendFile(path.resolve('./views/berners-lee.html'));
+})
+app.get('/clarke',(req, res) => {
+    res.sendFile(path.resolve('./views/clarke.html'));
+})
+app.get('/hamilton',(req, res) => {
+    res.sendFile(path.resolve('./views/hamilton.html'));
+})
+app.get('/hopper',(req, res) => {
+    res.sendFile(path.resolve('./views/hopper.html'));
+})
+app.get('/turing',(req, res) => {
+    res.sendFile(path.resolve('./views/turing.html'));
+})
 
-app.listen(port, () =>{
-    console.log(
-      `Servidor corriendo en el puerto ${port}`);
+app.listen(3030, () => {
+    console.log("el servi está corriendo")
 })
